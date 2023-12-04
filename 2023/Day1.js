0403c5d1f4be4f84545d6212b6928df54edb2373
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-12-04 15:17:54 -0500

    day 1 plan of attack

--- 2023/Day1.js
+++ 2023/Day1.js
@@ -1,1 +1,16 @@
 // link : https://adventofcode.com/2023/day/1
+
+/* Plan of Attack 
+    - List = Array 
+    - Create variable = store numbers
+    - Loop through my array of inputs
+    - Each input, check for number (two pointers)
+    - Reduce function on variable of numbers 
+    - Return
+
+
+--> base cases <--
+    - if only one digit, use it twice
+    - if no digits = 0; 
+    - if more than two digits, only take first & last
+*/ 
